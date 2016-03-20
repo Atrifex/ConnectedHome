6894dc2ad1ee9f071dbe48f1f83019874c97b7a3
DoItForThePi <rishirt.us@gmail.com>
2016-03-20 13:08:30 -0500

    iOS App Started
    Decided to use a tabbed view template for the app. This will allow for
    best user interface.

---
+++ Project Code/Native App/iOS App/Connected Home/Connected Home/AppDelegate.swift
@@ -0,0 +1,66 @@
+//
+//  AppDelegate.swift
+//  Connected Home
+//
+//  Created by Rishi Thakkar on 3/20/16.
+//  Copyright © 2016 DoItForThePi. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        print("application Method has been called")
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+        
+        print("applicationWillResignActive Method has been called")
+        
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        print("applicationDidEnterBackground Method has been called")
+        
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        
+        print("applicationWillEnterForeground Method has been called")
+        
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        print("applicationDidBecomeActive Method has been called")
+        
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        
+        
+        print("applicationWillTerminate Method has been called")
+        
+        
+    }
+
+
+}
+
